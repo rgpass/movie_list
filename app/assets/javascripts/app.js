90650be8339c07fcf51808c5c25a3e7240cefb93
rgpass <rgpass@gmail.com>
2014-11-20 09:09:56 -0500

    Fix dependency injection

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,5 @@
 angular.module('movieListApp', [])
-.service('movieService', function($http) {
+.service('movieService', ["$http", function($http) {
   this.search = function(title) {
     // Can either do it as a query string param
     // return $http.get('/search.json', { params: { title: title }});
@@ -7,7 +7,7 @@ angular.module('movieListApp', [])
     // Or can send it as a header -- which will conceal it (slightly) in the Network panel
     return $http.get('/search.json', { headers: { title: title }});
   };
-})
+}])
 .controller('testController', ["$scope", "movieService", function($scope,movieService) {
   $scope.submit = function() {
     movieService.search($scope.movieTitle).success(function(data) {
